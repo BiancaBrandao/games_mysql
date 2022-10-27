28198f32f5a6ba0a5748b7d4e11779b6916cfa9c
BiancaBrandao <biancabrandao199@gmail.com>
2022-10-27 02:57:50 +0000

    Generos

---
+++ app/src/main/java/biancabrandao/repositories/GeneroRepository.java
@@ -0,0 +1,9 @@
+package biancabrandao.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import biancabrandao.models.Genero;
+
+public interface GeneroRepository extends CrudRepository<Genero, Integer> {
+    
+}
