c2d6a991ca1f31f3341934cb845ff0f2f56ed701
BiancaBrandao <biancabrandao199@gmail.com>
2022-11-09 17:52:00 +0000

    Repositories

---
+++ app/src/main/java/biancabrandao/controllers/JogosController.java
@@ -0,0 +1,10 @@
+package biancabrandao.controllers;
+
+import org.springframework.stereotype.Controller;
+
+import biancabrandao.repositories.JogoRepository;
+
+@Controller
+public class JogosController {
+    private JogoRepository jogosRepo;
+}
---
+++ app/src/main/java/biancabrandao/repositories/JogoRepository.java
@@ -0,0 +1,9 @@
+package biancabrandao.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import biancabrandao.models.Jogo;
+
+public interface JogooRepository extends CrudRepository<Jogo, Integer> {
+    
+}
